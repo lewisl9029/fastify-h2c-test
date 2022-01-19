d551bbfb6330eb0c03761d40681007562e062027
Lewis Liu <lewisl9029@users.noreply.github.com>
2022-01-18 20:26:26 -0800

    Update to test concurrent requests

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-import * as fastify_ from 'fastify'
+import * as fastify_ from "fastify";
+import * as fs_ from "fs";
 
 const fastify = fastify_.default({
-  http2: true,
   logger: true,
-})
+});
 
-fastify.get('/', async () => 'hi')
-;(async () => {
+const index = fs_.promises.readFile("./index.html");
+
+fastify.get("/", async (request, response) => {
+  response.headers({
+    "content-type": "text/html",
+  });
+
+  return index;
+});
+
+fastify.get("/noop", () => "hi");
+(async () => {
   try {
-    await fastify.listen(58080, '::')
+    await fastify.listen(58080, "::");
   } catch (error) {
-    fastify.log.error(error)
-    process.exit(1)
+    fastify.log.error(error);
+    process.exit(1);
   }
-})()
+})();
